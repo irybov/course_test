e2f590cef55c6829ddfd133dfe79e6911d9d3df0
Ivan <v_cho@list.ru>
2021-06-20 12:56:03 +0300

    Task 7 done

--- src/main/java/ru/parsentev/task_007/Expression.java
+++ src/main/java/ru/parsentev/task_007/Expression.java
@@ -3,6 +3,8 @@ package ru.parsentev.task_007;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ru.parsentev.task_001.Calculator;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -21,6 +23,28 @@ public class Expression {
     }
 
     public double calc() {
-        throw new UnsupportedOperationException();
+    	if(expr.length()!=3) {
+        throw new IllegalStateException();
+    	}
+    	Calculator calculator = new Calculator();
+    	String[] values = expr.split("");
+    	double a = Double.valueOf(values[0]);
+    	double b = Double.valueOf(values[2]);
+    	if(values[1].equals("+")) {
+    		calculator.add(a, b);
+    	}
+    	else if(values[1].equals("-")) {
+    		calculator.substract(a, b);
+    	}
+    	else if(values[1].equals("/")) {
+    		calculator.div(a, b);
+    	}
+    	else if(values[1].equals("*")) {
+    		calculator.multiple(a, b);
+    	}
+    	else {
+    		calculator.expand(a, b);
+    	}
+    	return calculator.getResult();
     }
 }
